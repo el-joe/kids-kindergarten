415c8b26989c5d1a18f3ed71b7e271b23cf254a2
el-joe <eljoe1717@gmail.com>
2018-04-30 22:58:17 +0200

    Still Designing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -276,53 +276,44 @@
                 <center><h2 class="best">Moment from kids</h2></center>
                 <br>
                 <div class="row">
-                    <div class="col-sm-6 center-block">
-                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                            <!-- Indicators -->
-                            <ol class="carousel-indicators">
-                              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                              <li data-target="#myCarousel" data-slide-to="1"></li>
-                              <li data-target="#myCarousel" data-slide-to="2"></li>
-                            </ol>
-
-                            <!-- Wrapper for slides -->
-                            <div class="carousel-inner">
-
-                              <div class="item active">
-                                <img src="http://placehold.it/300x300" alt="Los Angeles" style="width:100%;">
-                                <div class="carousel-caption">
-                                  <h3>Los Angeles</h3>
-                                  <p>LA is always so much fun!</p>
-                                </div>
+                    <div class="col-sm-6 mx-auto">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                          <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                          </ol>
+                          <div class="carousel-inner">
+                            <div class="carousel-item active">
+                              <img class="d-block w-100" src="http://placehold.it/300x300" alt="First slide">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h5>...</h5>
+                                <p>...</p>
                               </div>
-
-                              <div class="item">
-                                <img src="http://placehold.it/300x300" alt="Chicago" style="width:100%;">
-                                <div class="carousel-caption">
-                                  <h3>Chicago</h3>
-                                  <p>Thank you, Chicago!</p>
-                                </div>
-                              </div>
-                            
-                              <div class="item">
-                                <img src="http://placehold.it/300x300" alt="New York" style="width:100%;">
-                                <div class="carousel-caption">
-                                  <h3>New York</h3>
-                                  <p>We love the Big Apple!</p>
-                                </div>
-                              </div>
-                          
                             </div>
-
-                            <!-- Left and right controls -->
-                            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                              <span class="glyphicon glyphicon-chevron-left"></span>
-                              <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                              <span class="glyphicon glyphicon-chevron-right"></span>
-                              <span class="sr-only">Next</span>
-                            </a>
+                            <div class="carousel-item">
+                              <img class="d-block w-100" src="http://placehold.it/300x300" alt="Second slide">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h5>...</h5>
+                                <p>...</p>
+                              </div>
+                            </div>
+                            <div class="carousel-item">
+                              <img class="d-block w-100" src="http://placehold.it/300x300" alt="Third slide">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h5>...</h5>
+                                <p>...</p>
+                              </div>
+                            </div>
+                          </div>
+                          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                          </a>
                         </div>
                     </div>
                 </div>
