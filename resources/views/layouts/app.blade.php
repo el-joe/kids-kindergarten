dfd712a42cec94249b8c423a4bec82ee1dde7b44
el-joe <eljoe1717@gmail.com>
2018-04-30 21:16:08 +0200

    Working With Design

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    @yield('style')
+
+</head>
+<body>
+
+{{-- responsive navBar --}}
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+  <a class="navbar-brand kids" href="#">Kids</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="nav navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT US</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CLASSES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">TEACHERS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">GALLERY</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT US</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<br>
+
+
+<div class="content">
+    @yield('content')
+</div>
+
+<script
+  src="http://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,52 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3 .col-md-3 footer-p1">
+                    <p><strong class="footer-logo">kids</strong> <small>kindergarten</small></p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc lacinia.
+                    </p>
+
+                </div>
+                <div class="col-sm-3 .col-md-2 footer-p2">
+                    <p class="footer-head">- Contact Info :</p>
+                    <ul>
+                        <li><i class="fas fa-map-marker-alt" style="color: white; font-size: 12px;"></i> 99 S.t Jomblo Park Pekanbaru 28292. Indonesia</li>
+                        <li><i class="fas fa-phone" style="color: white; font-size: 12px;"></i> (0761) 654-123987</li>
+                        <li><i class="fas fa-envelope" style="color: white; font-size: 12px;"></i>info@yoursite.com</li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 .col-md-2 footer-p3">
+                    <p class="footer-head">- Useful Links :</p>
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Our Teacher</a></li>
+                        <li><a href="#">Our Classes</a></li>
+                        <li><a href="#">Our Events</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 .col-md-4 footer-p4">
+                    <p class="footer-head">- Get in Touch :</p>
+                    <br>
+                    <ul>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                            <span class="social-media">
+                                <a href="#">&nbsp<i class="fab fa-facebook-f"></i>&nbsp&nbsp</a>
+                                <a href="#">&nbsp<i class="fab fa-twitter"></i>&nbsp</a>
+                                <a href="#">&nbsp<i class="fab fa-instagram"></i>&nbsp</a>
+                                <a href="#">&nbsp<i class="fab fa-linkedin-in"></i>&nbsp</a>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-12">
+                    <center><p>Copyright Kids 2017. All Rights Reserved. By <a href="https://www.facebook.com/youssef.magdy.127648">el-joe</a></p></center>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
